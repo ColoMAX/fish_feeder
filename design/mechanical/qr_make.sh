7e7601f78f9712e1eede7eba60522080d6e31742
colomax <max.suurland@gmail.com>
2024-01-15 20:31:06 +0100

    fix weird page toc

--- design/mechanical/qr_make.sh
+++ design/mechanical/qr_make.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qrencode --verbose -iv 1 -l L -t SVG --background=00000000 --foreground=FFFF00 -o qr.svg "https://ColoMAX.github.io/fish_feeder"
+qrencode --verbose -iv 1 -l L -t SVG --background=00000000 --foreground=FFFF00 -o qr.svg "https://github.com/ColoMAX/fish_feeder"
